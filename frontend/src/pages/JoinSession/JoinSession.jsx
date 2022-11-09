c06a1fcb077ae3fe912b471fac581c62f67de26b
Christian Jay Anunciado <iamblank015@gmail.com>
2022-11-09 10:43:59 +0800

    Added Module 6 - Added join session - Added create session - Added group video call - Added camera and video controls - Styled components and UI

--- frontend/src/pages/JoinSession/JoinSession.jsx
+++ frontend/src/pages/JoinSession/JoinSession.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
-import { useNavigate, useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
+import { useNavigate, useParams } from 'react-router-dom'
 
 
 function JoinSession(props) {
