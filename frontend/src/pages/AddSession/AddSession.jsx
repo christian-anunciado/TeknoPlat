58419a4b952bcd05d2392e4784fd1814848370a2
vhanbri <vhannebrianvillaso@gmail.com>
2022-11-10 11:35:19 +0800

    Update the Add Session Page

--- frontend/src/pages/AddSession/AddSession.jsx
+++ frontend/src/pages/AddSession/AddSession.jsx
@@ -56,14 +56,39 @@ const AddSession = () => {
   console.log(session);
   return (
     <>
-      <Navbar />
-      <div className='createSession-container'>
-        <h1> Create Session </h1>
-        <input type="text" name="sessionName" value={session.sessionName} placeholder="Session Name" onChange={handleChange} required />
-        <input type="password" name="sessionPassword" value={session.sessionPassword} placeholder="Password" onChange={handleChange} required />
-        <input type="datetime-local" name="startsAt" value={session.startsAt} placeholder="Date Start and Time" onChange={handleChange} required />
-        <input type="text" name="sessionDescription" value={session.sessionDescription} placeholder="Session Description" onChange={handleChange} required />
-        <input className='submit-button' type="submit" value="Create Session" onClick={handleSubmit} />
+
+      <Navbar/>
+      <div className="add-session">
+      <div className="container">
+    <div className="title">Create Session</div>
+    <div className="content">
+      <form >
+        <div className="user-details">
+          <div className="input-box">
+            <span className="details">Session Name</span>
+            <input type="text" name='sessionName' value={session.sessionName} placeholder="Name" onChange={handleChange} required/>
+          </div>
+          <div className="input-box">
+            <span className="details">Date and Time</span>
+            <input type="datetime-local" name="startsAt" value={session.startsAt}placeholder="Select date and time" onChange={handleChange} required/>
+          </div>
+          <div className="input-box">
+            <span className="details">Password</span>
+            <input type="password" name="sessionPassword" value={session.sessionPassword} placeholder="Set password" onChange={handleChange}  required/>
+            
+          </div>
+          <div className="input-box">
+            <span className="details">Session Details</span>
+            <input type="text" name="sessionDescription" value={session.sessionDescription} placeholder="Enter session details" onChange={handleChange}required/>
+          </div>
+        </div>
+        <div className="button">
+          <input type="submit" value="Create" onClick={handleSubmit}/>
+        </div>
+      </form>
+    </div>
+  </div>
+
       </div>
     </>
   )
