e464f437c4571acd24c1d051781523552c143511
JohnFerrolino <Ferrolinojohn@gmail.com>
2022-11-20 22:39:28 +0800

    Added Rating UI and Session UI

--- frontend/src/pages/JoinSession/Session.jsx
+++ frontend/src/pages/JoinSession/Session.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
+import "./Session.scss"
 
 
 function Session() {
@@ -15,7 +16,7 @@ function Session() {
     }, [])
 
     const fetchApi = async () => {
-        const response = await axios.get('http://127.0.0.1:8000/api/joinsession')
+        const response = await axios.get('http://127.0.0.1:8000/api/joinsession/1')
         const data = await response.data
         setApi(data)
     }
@@ -35,29 +36,43 @@ function Session() {
 
     return (
         <div>
-            <Navbar />
-            <h1>SESSIONS: </h1>
-            <input type="text"
-                placeholder="Search Session ID"
-                name="search" value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
-            <Link to={`/joinsession/${search}`} ><button>Search</button></Link>
-            {/* {api.map((apis) => {
-                return <div>
-                <h1 key={apis.id}>{apis.sessionName}</h1>
-                <p key={apis.id}>{apis.sessionDescription}</p>   
-                <input type="text" 
-                    placeholder="Enter Password" 
-                    name="password" value={password} 
-                    onChange = {(e) => setPassword(e.target.value)} 
-                    />
-                <Link to={`/insession/${apis.id}`} ><button onClick={joinSession}>Join</button></Link>
-                <Link to={`/insession/${apis.id}`} ><button disabled>Leave</button></Link>
-                <br/><br/>             
+            <Navbar/>
+            <div className='session-space'>
+                <div className="session-container">
+                    <div className='session-1 session button'>
+                        <input type="text"
+                            placeholder="Search Session ID"
+                            name="search" value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                        <Link to={`/joinsession/${search}`} ><button>Search</button></Link>
+                    </div>
+                    <div className='session-2 session'>
+                        <div className='session-2-1'>
+                            <h1>Picture</h1>
+                        </div>
+                        {api.map((apis) => {
+                        return <div className='session-2-2'>
+                            <div className='session-2-2-1'>
+                                <h1 key={apis.id}>{apis.sessionName}</h1>
+                            </div>
+                            <div className='session-2-2-2'>
+                                <p key={apis.id}>{apis.sessionDescription}</p>    
+                            </div>
+                            <div className='session-2-2-3'>
+                                <div className='session-2-2-3-1 button-shaded'>
+                                    <Link to={`/insession/${apis.id}`} ><button onClick={joinSession}>Join</button></Link>
+                                    </div>
+                                <div className='session-2-2-3-2 button-noshade'>
+                                    <Link to={`/insession/${apis.id}`} ><button disabled>Leave</button></Link>
+                                </div>
+                            </div>
+                        </div>
+                    })}
+                    </div>
                 </div>
-            })} */}
-        </div >
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/Rating/RatingSession.jsx
+++ frontend/src/pages/Rating/RatingSession.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import "./RatingSession.scss"
 
 const RatingSession = () => {
     const [punctuality, setPunctuality] = useState("")
@@ -46,16 +47,16 @@ const RatingSession = () => {
   return (
     <>
     <Navbar/>
-    <div className="add-session">
-    <div className="container">
-    <div className="title">Rate</div>
-    <div className="title">Pitch</div>
-    <div className="content">
-      <form >
-        <div className="user-details">
-          <div className="input-box">
+    <div className="rating-space">
+      <div className="rating-container">
+        <div className = "items-1 item">
+          <div className = "items-1-1 item">Rate</div>
+          <div className = "items-1-2 item">Close</div>
+        </div>
+        <div className = "items-2 item">Session Title</div>
+        <div className = "items-3 item">
+          <div className = "items-3-1 item item-direction">
             <label for="punctuality">Punctuality</label>
-            <br></br>
             <select name="Punctuality" id="punctuality" required onChange={(e) => setPunctuality(e.target.value)} >
             <option disabled selected hidden color required>Select Option</option>
               <option value="1">1</option>
@@ -65,9 +66,8 @@ const RatingSession = () => {
               <option value="5">5</option>
             </select>
           </div>
-          <div className="input-box">
-            <label for="presentation">Presentation</label>
-            <br></br>
+          <div className = "items-3-2 item item-direction">
+            <label label for="presentation">Presentation</label>
             <select name="Presentation" id="presentation" onChange={(e) => setPresentation(e.target.value)} >
             <option disabled selected hidden color required>Select Option</option>
               <option value="1">1</option>
@@ -77,10 +77,10 @@ const RatingSession = () => {
               <option value="5">5</option>
             </select>
           </div>
-          <div className="input-box">
-          <div className="selectdiv">
+        </div>
+        <div className = "items-4 item">
+          <div className = "items-4-1 item item-direction">
             <label for="delivery">Delivery</label>
-            <br></br>
             <select name="Delivery" id="delivery" onChange={(e) => setDelivery(e.target.value)} >
             <option disabled selected hidden color required>Select Option</option>
               <option value="1">1</option>
@@ -89,11 +89,9 @@ const RatingSession = () => {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
-            </div>
           </div>
-          <div className="input-box">
+          <div className = "items-4-2 item item-direction">
             <label for="innovativeness">Innovativeness</label>
-            <br></br>
             <select name="Innovativeness" id="innovativeness" onChange={(e) => setInnovativeness(e.target.value)} >
             <option disabled selected hidden color required>Select Option</option>
               <option value="1">1</option>
@@ -103,19 +101,18 @@ const RatingSession = () => {
               <option value="5">5</option>
             </select>
           </div>
-          <div className="input-box">
-            <span className="details">Feedback</span>
-            <input type="text" name="Feedback" placeholder="Feedback here" onChange={(e) => setFeedback(e.target.value)}/>
+        </div>
+        <div className = "items-5 item">
+          <div className = "items-5-1 item item-direction">
+            <label className="details">Feedback</label>
+            <textarea type="text" name="Feedback" placeholder="Feedback here" onChange={(e) => setFeedback(e.target.value)}/>
           </div>
         </div>
-        <div className="button">
+        <div className = "items-6 item button">
           <input type="submit" value="Submit" onClick = {rateSession}/>
         </div>
-      </form>
-    </div>
-  </div>
-
       </div>
+    </div>
     </>
   )
 }
