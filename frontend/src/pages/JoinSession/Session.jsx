9c528d2a33bb76c75c3bcef003d7622654b08328
JohnFerrolino <Ferrolinojohn@gmail.com>
2022-11-21 11:50:25 +0800

    Fixed Minor Issues and added modal to joinsession

--- frontend/src/pages/JoinSession/Session.jsx
+++ frontend/src/pages/JoinSession/Session.jsx
@@ -4,13 +4,22 @@ import { useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
+import { useNavigate, useParams } from 'react-router-dom'
 import "./Session.scss"
+import "./JoinSessionModal.scss"
 
 
 function Session() {
     const [api, setApi] = useState([])
     const [search, setSearch] = useState([])
     const [password, setPassword] = useState([])
+
+
+    //Temp
+    const [passwordStatus, setPasswordStatus] = useState(false)
+    const navigate = useNavigate()
+    const [modal, setModal] = useState(false);
+    
     useEffect(() => {
         fetchApi()
     }, [])
@@ -32,6 +41,36 @@ function Session() {
         });
     }
 
+
+    //Temp
+    const toggleModal = () =>{
+        setModal(!modal);
+    };
+
+    if(modal){
+        document.body.classList.add('active-modal')
+    }else{
+        document.body.classList.remove('active-modal')
+    }
+
+    const handleJoin = (e) => {
+        e.preventDefault()
+
+        setPasswordStatus(true)
+        toggleModal()
+
+    }
+
+    const handlePasswordSubmit = (e) => {
+        e.preventDefault()
+        if (password === api[0].sessionPassword) {
+            navigate(`/session`)
+        } else {
+            alert("Incorrect Password!")
+        }
+
+    }
+
     console.log('api: ', typeof (api));
 
     return (
@@ -48,20 +87,45 @@ function Session() {
                         <Link to={`/joinsession/${search}`} ><button>Search</button></Link>
                     </div>
                     <div className='session-2 session'>
-                        <div className='session-2-1'>
-                            <h1>Picture</h1>
-                        </div>
+                        <div className='session-2-1'>Picture</div>
                         {api.map((apis) => {
                         return <div className='session-2-2'>
                             <div className='session-2-2-1'>
-                                <h1 key={apis.id}>{apis.sessionName}</h1>
+                                <div className="session-2-2-1" key={apis.id}>{apis.sessionName}</div>
                             </div>
                             <div className='session-2-2-2'>
-                                <p key={apis.id}>{apis.sessionDescription}</p>    
+                                <div className="session-2-2-2" key={apis.id}>{apis.sessionDescription}</div>
                             </div>
                             <div className='session-2-2-3'>
                                 <div className='session-2-2-3-1 button-shaded'>
-                                    <Link to={`/insession/${apis.id}`} ><button onClick={joinSession}>Join</button></Link>
+                                    {/* <Link to={`/insession/${apis.id}`} ><button onClick={joinSession}>Join</button></Link> */}
+                                    <button onClick={handleJoin}>Join</button>
+
+                                    {modal && (
+                                        <div className="modal">
+                                            <div className="overlay">
+                                                <div className="modal-content">
+                                                    <div className="verifypassword">
+                                                        <div className="verifypassword-1">
+                                                            <button onClick={toggleModal}>Close</button>
+                                                        </div>
+                                                        <div className="verifypassword-2">
+                                                                {passwordStatus &&
+                                                                    <>
+                                                                    <div className="verifypassword-2-1">
+                                                                        <input type="password" name="password" value={password} placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
+                                                                    </div>
+                                                                    <div className="verifypassword-2-2">
+                                                                        <button onClick={handlePasswordSubmit}>Join</button>
+                                                                    </div>
+                                                                    </>
+                                                                }
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
                                     </div>
                                 <div className='session-2-2-3-2 button-noshade'>
                                     <Link to={`/insession/${apis.id}`} ><button disabled>Leave</button></Link>
