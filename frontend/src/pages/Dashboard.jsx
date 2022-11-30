3ee2553aa0ee1260b4b74739f78c985b7b6a4201
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2022-11-30 21:44:41 +0800

    Revert "Merge pull request #32 from SkrowRepap/dev/anunciado"
    This reverts commit 42cd2361419ba6a4affdd4b5518e4683d21d799a, reversing
    changes made to ddaa06b3b66e1ea115faae5d838a7bad39bed439.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,11 @@
-import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import {Link} from 'react-router-dom'
-import Navbar from '../components/Navbar/Navbar'
 
 const Dashboard = ({name}) => {
-    const [session,setSession] = useState([]);
+
     const [username,setUsername] = useState("")
-    let form = new FormData();
 
     useEffect(() => {
         setUsername(name)
@@ -19,31 +16,13 @@ const Dashboard = ({name}) => {
             headers: {'Content-Type': 'application/json'},
             credentials: 'include',
         });
-    }     
-    useEffect(()=>{
-        const fetchSession = async () => {
-            const response = await axios.get('http://localhost:8000/api/joinsession')
-            setSession(response.data)
-        }
-        fetchSession()
-    },[])    
-    
+    }
     return (
         <div>
-            <Navbar/>
-            <h1>Dashboard</h1>
-         
+            <h1>Sample Page</h1>
+            <h3>Sample Api Call:</h3>
             <h1> Welcome: {name} </h1>
             <Link to="/login" onClick={logout}>Logout</Link>
-            <h1>Sessions:</h1>
-            {session.map((sessions, index) => {
-                       
-                            return (
-                              <p>{sessions.sessionName}</p>
-                            );
-                          
-                         
-                        })}
         </div>
     )
 }
