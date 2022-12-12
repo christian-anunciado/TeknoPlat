60bcc7f3c29c73f46a25eff6017789b4c40c2ed1
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2022-12-12 16:39:33 +0800

    Handle Password Submit on Dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, axios } from 'react'
 import { useEffect } from 'react'
 import { useContext } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
 import AuthContext from '../../context/AuthContext'
 
@@ -10,18 +10,20 @@ import Sidebar from '../../components/sidebar/Sidebar'
 
 import './Dashboard.scss'
 import Datatable from '../../components/datatable/Datatable'
+import Notification from '../../components/Notifcation/Notifcation'
 const Dashboard = () => {
-    return (
-      <>
-     
+  return (
+    <>
+
       <div className="home">
-      <Sidebar />
-      <div className="homeContainer">
-        <Navbar />
-      <Datatable/>
+        <Sidebar />
+        <div className="homeContainer">
+          <Navbar />
+          <Datatable />
+        </div>
+        <Notification />
       </div>
-    </div>
-      </>
-    )
+    </>
+  )
 }
 export default Dashboard
