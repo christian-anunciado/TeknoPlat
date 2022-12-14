380e04d4493c98a30d26167616a4bbe0213baf65
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2022-12-14 20:34:04 +0800

    Change manifest.json

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -15,12 +15,7 @@ export const AuthProvider = ({ children }) => {
 
     let loginUser = async (e) => {
         e.preventDefault()
-        let response = await Api.post('api/token/', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 'email': e.target.email.value, 'password': e.target.password.value })
-        })
+        let response = await Api.post('api/token/', { email: e.target.email.value, password: e.target.password.value })
         let data = await response.json()
         if (response.status === 200) {
             setAuthToken(data)
@@ -38,12 +33,7 @@ export const AuthProvider = ({ children }) => {
     }
 
     let updateToken = async () => {
-        let response = await Api.post('api/token/refresh/', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 'refresh': authToken?.refresh })
-        })
+        let response = await Api.post('api/token/refresh/', { refresh: authToken?.refresh })
         let data = await response.json()
         if (response.status === 200) {
             setAuthToken(data)
