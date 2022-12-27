ee24fbab169b2fee571ee841c6b97fa2b8b3ab46
JohnFerrolino <Ferrolinojohn@gmail.com>
2022-12-27 13:33:44 +0800

    Added Admin Dashboard

---
+++ frontend/src/components/admindatable/Admindatatablesource.js
@@ -0,0 +1,83 @@
+
+
+export const userColumns = [
+  {
+    field: "room",
+    headerName: "Session Name",
+    width: 250,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+
+          {params.row.room}
+        </div>
+      );
+    },
+  },
+  {
+    field: "details",
+    headerName: "Details",
+    width: "700",
+    editable: true,
+
+  },
+
+  {
+    field: "date",
+    headerName: "Starts at",
+    width: 250,
+    renderCell: (params) => {
+      var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+      var formattedDate = new Date(params.row.date)
+      return (
+        <div className="cellWithImg">
+          {formattedDate.toLocaleDateString("en-US", options)}
+        </div>
+      );
+    },
+  },
+
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+    renderCell: (params) => {
+      let statusText = ''
+      let statusClass = ''
+      switch (params.row.status) {
+        case 1:
+          statusClass = 'Active'
+          statusText = 'Active'
+          break;
+
+        case 2:
+          statusClass = 'Active'
+          statusText = 'Live'
+          break;
+
+        case 3:
+          statusClass = 'Inactive'
+          statusText = 'Ended'
+          break;
+
+        default:
+          break;
+      }
+
+      if (statusText === 'Active' && params.row.creator === params.row.user) {
+        return (
+          <div className={`cellWithStatus Host`}>
+            Host
+          </div>
+        );
+      } else {
+        return (
+          <div className={`cellWithStatus ${statusClass}`}>
+            {statusText}
+          </div>
+        );
+      }
+    },
+  },
+];
+
